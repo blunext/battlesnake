07a890307488bc1053de53d67985669096423ad9
Blunext <>
2021-07-02 12:41:57 +0200

    tile refactoring

--- game/tile.go
+++ game/tile.go
@@ -34,23 +34,18 @@ func (t *Tile) PathNeighbors() []astar.Pather {
 	for _, next := range newMoves() {
 		c := Coord{X: t.X + next.x, Y: t.Y + next.y}
 		neighborTile, present := t.board[c]
-		switch present {
-		case true:
-			//fmt.Printf("%d, %d\n", c.X, c.Y)
-			neighbors = append(neighbors, &neighborTile)
-		case false:
+		if !present {
 			fmt.Printf("%d, %d\n", c.X, c.Y)
 			neighborTile = Tile{Coord: &c, board: t.board, costIndex: empty}
 			t.board[c] = neighborTile
-			neighbors = append(neighbors, &neighborTile)
 		}
+		neighbors = append(neighbors, &neighborTile)
 	}
 	return neighbors
 }
 
 func (t *Tile) PathNeighborCost(to astar.Pather) float64 {
-	tile := to.(*Tile)
-	return tile.Cost()
+	return to.(*Tile).Cost()
 }
 
 func (t *Tile) PathEstimatedCost(to astar.Pather) float64 {
