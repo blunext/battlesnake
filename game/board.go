cc3b947e326dd69731535415a805b009dd281abb
Blunext <>
2021-07-11 16:18:29 +0200

    minor refactoring

--- game/board.go
+++ game/board.go
@@ -1,53 +1,5 @@
 package game
 
-type Game struct {
-	ID      string `json:"id"`
-	Timeout int32  `json:"timeout"`
-}
-
-type Coord struct {
-	X int `json:"X"`
-	Y int `json:"Y"`
-}
-
-type Battlesnake struct {
-	ID      string  `json:"id"`
-	Name    string  `json:"name"`
-	Health  int32   `json:"health"`
-	Body    []Coord `json:"body"`
-	Latency int32   `json:"latency"`
-	Head    Coord   `json:"head"`
-	Length  int32   `json:"length"`
-	Shout   string  `json:"shout"`
-}
-
-type Board struct {
-	Height int           `json:"height"`
-	Width  int           `json:"width"`
-	Food   []Coord       `json:"food"`
-	Snakes []Battlesnake `json:"snakes"`
-}
-
-type BattlesnakeInfoResponse struct {
-	APIVersion string `json:"apiversion"`
-	Author     string `json:"author"`
-	Color      string `json:"color"`
-	Head       string `json:"head"`
-	Tail       string `json:"tail"`
-}
-
-type GameRequest struct {
-	Game  Game        `json:"game"`
-	Turn  int         `json:"turn"`
-	Board Board       `json:"board"`
-	You   Battlesnake `json:"you"`
-}
-
-type MoveResponse struct {
-	Move  string `json:"move"`
-	Shout string `json:"shout,omitempty"`
-}
-
 type Direction struct {
 	X, Y    int
 	Heading string
--- main.go
+++ main.go
@@ -10,34 +10,6 @@ import (
 )
 
 func main() {
-	//
-	////moves := newMoves()
-	//g := game.GameRequest{}
-	//g.Board.Width = 11
-	//g.Board.Height = 11
-	////g.You.Head.X = 5
-	////g.You.Head.Y = 5
-	//
-	//snake := []pairs{
-	//	{0, 3}, {1, 3}, {2, 3}, {3, 3}, {4, 3}, {5, 3}, {5, 2}, {5, 1},
-	//}
-	//addSnake(&g, snake)
-	//snake = []pairs{
-	//	{1, 0}, {0, 0},
-	//}
-	//addYou(&g, snake)
-	//food := []game.Coord{
-	//	{8, 8}, {7, 6},
-	//}
-	//g.Board.Food = food
-	////fmt.Println(s)
-	//board := game.MakeBoard(g)
-	//
-	//game.FindFood(g.You.Head, board, g.Board.Food)
-	//moves := game.RankSpace(g.You.Head, board)
-	//
-	//fmt.Println(game.FindBest(moves))
-
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
